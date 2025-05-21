3ece71920913ff6c8c6596453dfe7e98e36bb0a9
Vital Alex <jbanteam@yandex.ru>
2025-05-21 23:22:25 +0300

    refactor(config): migrate jest and eslint config to typescript

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/../' }),
+};
+
+export default {
+  projects: [
+    {
+      displayName: 'Unit',
+      rootDir: 'src',
+      testMatch: ['<rootDir>/**/tests/**/*.test.ts'],
+      coverageDirectory: './coverage',
+      ...config,
+    },
+  ],
+};
