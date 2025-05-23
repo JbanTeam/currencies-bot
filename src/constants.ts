86b480a9b361c9eb89c3673035219c4269d0914e
Vital Alex <jbanteam@yandex.ru>
2025-05-23 13:24:22 +0300

    docs: update README and add .env example file

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,8 @@
 export const startMessage = `Привет! Я помогу вам узнать текущие курсы валют.\nНапишите /currency для получения списка доступных валют.`;
-export const helpMessage = 'Доступные команды: /start, /help, /currency.';
+
+export const helpMessage = `Доступные команды:
+/start - Описание
+/help - Помощь
+/currency - Доступные валюты`;
+
 export const currencyMessage = `Введите валютную пару в формате <b><u>USD-EUR</u></b>, чтобы узнать курс обмена.\n\n<b><u>Доступные валюты:</u></b>`;
